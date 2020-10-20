a8c437537b2057fa598399c95f5cab013b88b397
rzvxa <rzvxa@protonmail.com>
2020-10-20 12:04:10 +0330

    Added string extentions for NullOrEmpty and NullOrWhiteSpace

--- SystemTypes/StringExt.cs
+++ SystemTypes/StringExt.cs
@@ -4,5 +4,11 @@ namespace Utils.SystemTypes
     {
         public static string Format(this string @this, params object[] args) =>
             string.Format(@this, args);
+
+        public static bool NullOrEmpty(this string @this) =>
+            string.IsNullOrEmpty(@this);
+
+        public static bool NullOrWhiteSpace(this string @this) =>
+            string.IsNullOrWhiteSpace(@this);
     }
 }
